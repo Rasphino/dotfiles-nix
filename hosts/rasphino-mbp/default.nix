d8932f2435f655b84d89b7b7e897332dfacf7eef
Rasphino <im.lihh@outlook.com>
2022-11-14 19:48:42 +0800

    init nix

---
+++ hosts/rasphino-mbp/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+
+  # Make sure the nix daemon always runs
+  services.nix-daemon.enable = true;
+  
+  # if you use zsh (the default on new macOS installations),
+  # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
+  programs.zsh.enable = true;
+  # bash is enabled by default
+
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    # updates homebrew packages on activation,
+    # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
+    # onActivation.upgrade = true;
+    casks = [
+      "hammerspoon"
+      "raycast"
+      "logseq"
+      "iina"
+    ];
+  };
+}
