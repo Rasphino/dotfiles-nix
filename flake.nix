8ff73720e7c21d295c6c7d762dd457baf70a6522
Rasphino <im.lihh@outlook.com>
2022-11-19 10:03:22 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Rasphino's Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # add the inputs declared above to the argument attribute set
+  outputs =
+  { self
+  , nixpkgs
+  , home-manager
+  , nur
+  , ... }@inputs:
+  {
+    nixosConfigurations."nixos-vm" = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux"; # "x86_64-darwin" if you're using a pre M1 mac
+      modules = [
+        ./hosts/nixos-vm/hardware-configuration.nix
+        ./hosts/nixos-vm/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          nixpkgs.overlays = [
+            nur.overlay
+          ];
+        }
+      ]; # will be important later
+    };
+  };
+}
