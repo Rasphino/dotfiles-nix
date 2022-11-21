abfac7bca53ed0dc63660d30a752d5f4af343915
Rasphino <im.lihh@outlook.com>
2022-11-21 10:44:04 +0800

    enable delta

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -4,5 +4,14 @@
     enable = true;
     userName = "Rasphino";
     userEmail = "im.lihh@outlook.com";
+    
+    delta.enable = true;
+    delta.options = {
+      side-by-side = true;
+    };
+
+    extraConfig = {
+      merge.conflictstyle = "diff3";
+    };
   };
 }
