49607a1a7f60ddda2d8a2628dac85bd6bb885c34
Rasphino <im.lihh@outlook.com>
2023-01-08 16:41:05 +0800

    saki-mk1: enable clash

--- hosts/saki-mk1/default.nix
+++ hosts/saki-mk1/default.nix
@@ -17,9 +17,8 @@
 
   networking.hostName = "saki-mk1"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # TODO: temporary use `rasphino-mbp` as gateway, setup its own clash proxy later.
-  networking.defaultGateway = "192.168.31.240";
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.defaultGateway = "192.168.31.1";
+  networking.nameservers = [ "127.0.0.1" ];
   networking.interfaces.enp0s25.ipv4.addresses = [{
     address = "192.168.31.30";
     prefixLength = 24;
@@ -68,8 +67,6 @@
     wget
     git
     gcc
-
-    clash-meta
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -84,12 +81,17 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  
+  services.clash-meta = {
+    enable = true;
+    configFile = "/root/clash-meta-config.yaml";
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
